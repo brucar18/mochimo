871598ca8ef46ebec564e4143937acba6614f8e1
Joey34kaze <ragnar1724@gmail.com>
2018-06-23 18:54:07 +0200

    update
    update

--- awsStart.sh
+++ awsStart.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+screen -S mochimoMiner
+
 git clone https://github.com/JoeY34kaze/mochimo
 chmod -R a+rwx mochimo
+
+while true
+do
 cd mochimo/mochi-beta/bin/
-nohup ./gomochi d -t3 > output.txt &
+nohup ./gomochi d -t3 > output.txt
+sleep 2
+done
