e0dfc22d1230ff4d4bf44bf4d12931857ea3380b
brucar18 <aljazbucar@gmail.com>
2018-06-23 21:02:04 +0200

    Update awsStart.sh

--- awsStart.sh
+++ awsStart.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
+##usage
+## curl -sSL https://raw.githubusercontent.com/JoeY34kaze/mochimo/master/awsStart.sh | bash 
+## A.B. 2018
 git clone https://github.com/JoeY34kaze/mochimo
 chmod -R a+rwx mochimo
-cd mochimo/mochi-beta/bin/
-#newline
-#while true
-#do
-#nohup ./gomochi d -t3 > mochiminer.log &
-#sleep 2
-#done
 
+cat << EOF > ~/runminer.sh
+#/bin/bash
+cd mochimo/mochi-beta/bin/
 ~/mochimo/mochi-beta/bin/gomochi d -t3 >> ~/mochi.log
+EOF
+screen -dmS Miner -c `~/runminer.sh`
