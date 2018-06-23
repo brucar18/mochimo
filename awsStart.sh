97fefdb7d315fec5d8548c386b2269670d8f5a95
Joey34kaze <ragnar1724@gmail.com>
2018-06-23 19:53:47 +0200

    Update awsStart.sh

--- awsStart.sh
+++ awsStart.sh
@@ -3,10 +3,10 @@
 
 git clone https://github.com/JoeY34kaze/mochimo
 chmod -R a+rwx mochimo
-
+cd mochimo/mochi-beta/bin/
 while true
 do
-cd mochimo/mochi-beta/bin/
+
 nohup ./gomochi d -t3 > mochiminer.log &
 sleep 2
 done
