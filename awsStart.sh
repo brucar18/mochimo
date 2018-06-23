bb55fbe942bcd9a49c2f3c4fe5b39e6a03a6999e
Joey34kaze <ragnar1724@gmail.com>
2018-06-23 19:06:13 +0200

    uhm
    uhm

--- awsStart.sh
+++ awsStart.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 screen -S mochimoMiner
-
 git clone https://github.com/JoeY34kaze/mochimo
 chmod -R a+rwx mochimo
-
-while true
+a=0
+while [ $a -lt 10 ]
 do
 cd mochimo/mochi-beta/bin/
 nohup ./gomochi d -t3 > output.txt
